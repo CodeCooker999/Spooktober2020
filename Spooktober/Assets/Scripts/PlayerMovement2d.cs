4ae7f91b65718f01309a99624d5e57c0a5eb5404
CodeCooker999 <72916970+CodeCooker999@users.noreply.github.com>
2020-10-24 23:21:49 +0600

    Update

--- Spooktober/Assets/Scripts/PlayerMovement2d.cs
+++ Spooktober/Assets/Scripts/PlayerMovement2d.cs
@@ -16,12 +16,12 @@ public class PlayerMovement2d : MonoBehaviour
 
     void Update()
     {
-        Vector2 mi = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
-        mv = mi.normalized * speed;
+
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + mv * Time.fixedDeltaTime);
     }
+        
 }
