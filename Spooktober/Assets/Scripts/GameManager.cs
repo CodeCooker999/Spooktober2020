5b356c8dd2a01d98a7e7fde11260a5dd1d26af8f
CodeCooker999 <72916970+CodeCooker999@users.noreply.github.com>
2020-10-28 15:37:36 +0600

    Updated Player using bolt made some animations and some polishing

---
+++ Spooktober/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GmaeManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void Resume()
+    {
+        Debug.Log("Resume");
+    }
+
+    public void Leave()
+    {
+        Debug.Log("Leave");
+    }
+
+    public void Hurt()
+    {
+        Debug.Log("Ouch");
+    }
+
+    public void Die()
+    {
+        Debug.Log("DEd");
+    }
+
+}
