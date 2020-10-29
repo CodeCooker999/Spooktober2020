395b3ba9aa299ab6eb2a5d86b157f2f5f7b015f3
CodeCooker999 <72916970+CodeCooker999@users.noreply.github.com>
2020-10-29 06:42:31 +0600

    update player

---
+++ Spooktober/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform player;
+    [SerializeField]
+    float agroRange;
+    [SerializeField]
+    float moveSpeed;
+
+    Rigidbody2D rb;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+        if(distToPlayer < agroRange)
+        {
+            ChasePlayer();
+        }
+        else if(distToPlayer < 0.6)
+        {
+            animator.SetBool("Attack", true);
+        }
+        else
+        {
+            StopChasePlayer();
+        }
+    }
+
+    void StopChasePlayer()
+    {
+        rb.velocity = Vector2.zero;
+        animator.SetBool("Walk", false);
+    }
+
+    void ChasePlayer()
+    {
+        if (transform.position.x < player.position.x)
+        {
+            rb.velocity = new Vector2(moveSpeed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-moveSpeed, 0);
+            transform.localScale = new Vector2(-0.5f, 0.5f);
+        }
+
+        animator.SetBool("Walk",true);
+
+    }
+}
